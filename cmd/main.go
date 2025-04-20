989d977b3be34d4e628606f5582a398b08591953
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-04-20 17:32:45 +0200

    cleanup(duckdb): removing duckdb to avoid problems for the moment

--- cmd/main.go
+++ cmd/main.go
@@ -1,46 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
-	"fmt"
-	_ "github.com/marcboeker/go-duckdb/v2"
 	"go-db-etl/pkg/config"
 	"go-db-etl/pkg/logging"
-	"log"
 )
 
 func main() {
 	logging.EtlLogger.Info("Starting " + config.Config.Name)
-
-	db, err := sql.Open("duckdb", "")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec(`CREATE TABLE people (id INTEGER, name VARCHAR)`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = db.Exec(`INSERT INTO people VALUES (42, 'John')`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var (
-		id   int
-		name string
-	)
-	row := db.QueryRow(`SELECT id, name FROM people`)
-
-	err = row.Scan(&id, &name)
-	if errors.Is(err, sql.ErrNoRows) {
-		log.Println("no rows")
-	} else if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("id: %d, name: %s\n", id, name)
 
 }
