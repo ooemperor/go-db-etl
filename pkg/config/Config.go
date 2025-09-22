e265ad10f35b01fe5ede4e4fcabbddd5e7bbb8d3
Michael Kaiser <michael.kaiser@emplabs.ch>
2025-09-22 23:49:29 +0200

    feat(config): adding batchsize for reader and writer as env variables

--- pkg/config/Config.go
+++ pkg/config/Config.go
@@ -1,19 +1,22 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"github.com/ooemperor/go-db-etl/pkg/sources"
 	"os"
 	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/ooemperor/go-db-etl/pkg/sources"
 )
 
 /*
 Configuration Object that holds all the necessary config information
 */
 type Configuration struct {
-	postgresHost string
-	timeout      int64
-	Name         string
+	postgresHost    string
+	timeout         int64
+	Name            string
+	BatchSizeReader int
+	BatchSizeWriter int
 }
 
 /*
@@ -27,6 +30,11 @@ func (conf *Configuration) Init(envFile string) (*Configuration, error) {
 
 	conf.timeout, _ = strconv.ParseInt(os.Getenv("timeout_sec"), 10, 64)
 	conf.Name = os.Getenv("name")
+	var batchReader, _ = strconv.ParseInt(os.Getenv("batch_size_read"), 10, 64)
+	var batchWriter, _ = strconv.ParseInt(os.Getenv("batch_size_writer"), 10, 64)
+
+	conf.BatchSizeReader = int(batchReader)
+	conf.BatchSizeWriter = int(batchWriter)
 
 	return conf, nil
 }
